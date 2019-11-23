02d9e79bb203ae9e1edcdc2d30a368f9eab2cfc6
OscarRivas99 <00005218@uca.edu.sv>
2019-11-23 11:41:24 -0600

    fixing mongo uri 3

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ let app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.json());
