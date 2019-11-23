88c4043041c1ad9de89fe478eddb0c1c0bd68001
OscarRivas99 <00005218@uca.edu.sv>
2019-11-23 11:13:39 -0600

    fixing mongo uri 2

--- config/conexion.js
+++ config/conexion.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://OscarR:root@cluster0-xxunr.mongodb.net/test?retryWrites=true&w=majority', { useMongoClient: true }); //mongodb+srv://OscarR:root@cluster0-xxunr.mongodb.net/test?retryWrites=true&w=majority
+mongoose.connect('mongodb://OscarR:root@cluster0-xxunr.mongodb.net/test?retryWrites=true&w=majority', { useMongoClient: true }); //mongodb+srv://OscarR:root@cluster0-xxunr.mongodb.net/test?retryWrites=true&w=majority
 
 module.exports = mongoose;
 //mongodb://localhost/crud-frutas-parcial
